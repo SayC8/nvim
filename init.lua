0ffc4f5bb517ad33a69fd34e20f94f3795d4b281
SayC8 <sayc8apex@gmail.com>
2026-02-16 10:48:08 +0000

    Fixed colorcheme/transparency

--- init.lua
+++ init.lua
@@ -7,18 +7,20 @@ for _, file in pairs(configs) do
   require("config." .. file)
 end
 
+vim.cmd("colorscheme retrobox")
+
 -- Transparent backgrounds
-local function fix_transparency()
-  local groups = {
-    "Normal", "NormalFloat", "SignColumn",
-  }
-  for _, group in ipairs(groups) do
-    vim.api.nvim_set_hl(0, group, { bg = "none", force = true })
-  end
-end
-
-fix_transparency()
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = fix_transparency,
-})
+-- local function fix_transparency()
+--   local groups = {
+--     "Normal", "NormalFloat", "SignColumn",
+--   }
+--   for _, group in ipairs(groups) do
+--     vim.api.nvim_set_hl(0, group, { bg = "none", force = true })
+--   end
+-- end
+--
+-- fix_transparency()
+--
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--   callback = fix_transparency,
+-- })
